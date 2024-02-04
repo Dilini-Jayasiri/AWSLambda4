940e58dee2b3a80fa4fbd1c64a40ed668a2c25c8
Dilini Jayasiri <dilininadeesha2018@gmail.com>
2024-02-04 23:50:46 +0530

    movie update function created

--- AWSLambda4/Function.cs
+++ AWSLambda4/Function.cs
@@ -80,6 +80,106 @@ namespace AWSLambda4
             return uniqueId;
         }
 
+        public async Task<APIGatewayHttpApiV2ProxyResponse> UpdateMovieAsync(APIGatewayHttpApiV2ProxyRequest request, ILambdaContext context)
+        {
+            try
+            {
+                var movieUpdateRequest = JsonConvert.DeserializeObject<Movie>(request.Body);
+
+                // Validate if required fields are present in the update request, handle validation as needed
+
+                AmazonDynamoDBClient client = new AmazonDynamoDBClient();
+                DynamoDBContext dbContext = new DynamoDBContext(client);
+
+                // Load the existing movie record from DynamoDB
+                Movie existingMovie = await dbContext.LoadAsync<Movie>(movieUpdateRequest.Id);
+
+                if (existingMovie == null)
+                { 
+                    return new APIGatewayHttpApiV2ProxyResponse
+                    {
+                        Body = "Movie not found",
+                        StatusCode = 404
+                    };
+                }
+
+                // Update the fields that are provided in the update request
+                if (!string.IsNullOrEmpty(movieUpdateRequest.MovieName))
+                {
+                    existingMovie.MovieName = movieUpdateRequest.MovieName;
+                }
+
+                if (!string.IsNullOrEmpty(movieUpdateRequest.ReleaseDate))
+                {
+                    existingMovie.ReleaseDate = movieUpdateRequest.ReleaseDate;
+                }
+
+                if (!string.IsNullOrEmpty(movieUpdateRequest.Rate))
+                {
+                    existingMovie.Rate = movieUpdateRequest.Rate;
+                }
+
+                if (!string.IsNullOrEmpty(movieUpdateRequest.Minutes))
+                {
+                    existingMovie.Minutes = movieUpdateRequest.Minutes;
+                }
+
+                if (!string.IsNullOrEmpty(movieUpdateRequest.Synopsis))
+                {
+                    existingMovie.Synopsis = movieUpdateRequest.Synopsis;
+                }
+
+                if (!string.IsNullOrEmpty(movieUpdateRequest.PosterURL))
+                {
+                    existingMovie.PosterURL = movieUpdateRequest.PosterURL;
+                }
+
+                if (!string.IsNullOrEmpty(movieUpdateRequest.Tagline))
+                {
+                    existingMovie.Tagline = movieUpdateRequest.Tagline;
+                }
+
+                if (!string.IsNullOrEmpty(movieUpdateRequest.Votes))
+                {
+                    existingMovie.Votes = movieUpdateRequest.Votes;
+                }
+
+                if (!string.IsNullOrEmpty(movieUpdateRequest.Genre))
+                {
+                    existingMovie.Genre = movieUpdateRequest.Genre;
+                }
+
+                if (!string.IsNullOrEmpty(movieUpdateRequest.BackdropURL))
+                {
+                    existingMovie.BackdropURL = movieUpdateRequest.BackdropURL;
+                }
+
+
+                // Add more fields to update as needed
+
+                // Save the updated movie record back to DynamoDB
+                await dbContext.SaveAsync(existingMovie);
+
+                var message = $"Movie with Id {existingMovie.Id} Updated";
+                LambdaLogger.Log(message);
+
+                return new APIGatewayHttpApiV2ProxyResponse
+                {
+                    Body = message,
+                    StatusCode = 200
+                };
+            }
+            catch (Exception ex)
+            {
+                return new APIGatewayHttpApiV2ProxyResponse
+                {
+                    Body = ex.Message,
+                    StatusCode = 400
+                };
+            }
+        }
+
+
         public async Task<APIGatewayHttpApiV2ProxyResponse> GetMovieByIdAsync(APIGatewayHttpApiV2ProxyRequest request, ILambdaContext context)
         {
             try
