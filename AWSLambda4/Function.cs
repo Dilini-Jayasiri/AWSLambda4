b4e36854def2c50158340920e7455cea44379ffe
Dilini Jayasiri <dilininadeesha2018@gmail.com>
2024-02-07 16:21:54 +0530

    updated

--- AWSLambda4/Function.cs
+++ AWSLambda4/Function.cs
@@ -5,7 +5,6 @@ using Amazon.DynamoDBv2.DataModel;
 using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.Core;
 using Newtonsoft.Json;
-using System.Collections.Generic;
 
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
@@ -16,174 +15,70 @@ namespace AWSLambda4
         private readonly AmazonDynamoDBClient _dynamoDbClient;
         private readonly DynamoDBContext _dbContext;
 
+
         public Function()
         {
             _dynamoDbClient = new AmazonDynamoDBClient();
             _dbContext = new DynamoDBContext(_dynamoDbClient);
         }
 
-        public async Task<APIGatewayHttpApiV2ProxyResponse> FunctionHandler(APIGatewayHttpApiV2ProxyRequest request, ILambdaContext context)
-        {
-            // Handling preflight requests
-            if (request.RequestContext.Http.Method == "OPTIONS")
-            {
-                return new APIGatewayHttpApiV2ProxyResponse
-                {
-                    StatusCode = 200,
-                    Headers = new Dictionary<string, string>
-                {
-                    { "Access-Control-Allow-Origin", "http://localhost:3000" },
-                    { "Access-Control-Allow-Headers", "Content-Type" },
-                    { "Access-Control-Allow-Methods", "OPTIONS,POST,GET" },
-                    { "Access-Control-Allow-Credentials", "true" },
-                },
-                };
-            }
 
+        public async Task<APIGatewayHttpApiV2ProxyResponse> GetAllMoviesAsync(APIGatewayHttpApiV2ProxyRequest request, ILambdaContext context)
+        {
             try
             {
-                // Your existing logic for handling different HTTP methods
-                if (request.RequestContext.Http.Method == "POST")
-                {
-                    return await CreateMovieAsync(request);
-                }
-                else if (request.RequestContext.Http.Method == "GET")
-                {
-                    if (request.PathParameters.ContainsKey("id"))
-                    {
-                        return await GetMovieByIdAsync(request);
-                    }
-                    else
-                    {
-                        return await GetAllMoviesAsync(request);
-                    }
-                }
-                else
-                {
-                    // Handle other HTTP methods if needed
-                    // Add more conditions as required
+                var data = await _dbContext.ScanAsync<Movie>(default).GetRemainingAsync();
 
-                    return new APIGatewayHttpApiV2ProxyResponse
-                    {
-                        Body = "Method not supported",
-                        StatusCode = 405,
-                        Headers = new Dictionary<string, string>
-                    {
-                        { "Access-Control-Allow-Origin", "http://localhost:3000" },
-                        { "Access-Control-Allow-Credentials", "true" },
-                    },
-                    };
-                }
+                return new APIGatewayHttpApiV2ProxyResponse
+                {
+                    Body = JsonConvert.SerializeObject(data),
+                    StatusCode = 200
+                };
             }
             catch (Exception ex)
             {
-                // Log the exception for debugging purposes
-                LambdaLogger.Log($"Error: {ex}");
-
                 return new APIGatewayHttpApiV2ProxyResponse
                 {
                     Body = ex.Message,
-                    StatusCode = 500,
-                    Headers = new Dictionary<string, string>
-                {
-                    { "Access-Control-Allow-Origin", "http://localhost:3000" },
-                    { "Access-Control-Allow-Credentials", "true" },
-                },
+                    StatusCode = 500
                 };
             }
         }
 
-        private async Task<APIGatewayHttpApiV2ProxyResponse> GetAllMoviesAsync(APIGatewayHttpApiV2ProxyRequest request)
+        public async Task<APIGatewayHttpApiV2ProxyResponse> CreateMovieAsync(APIGatewayHttpApiV2ProxyRequest request, ILambdaContext context)
         {
-            // Your existing logic for getting all movies
-            var data = await _dbContext.ScanAsync<Movie>(default).GetRemainingAsync();
-
-            return new APIGatewayHttpApiV2ProxyResponse
+            try
             {
-                Body = JsonConvert.SerializeObject(data),
-                StatusCode = 200,
-                Headers = new Dictionary<string, string>
-            {
-                { "Access-Control-Allow-Origin", "http://localhost:3000" },
-                { "Access-Control-Allow-Credentials", "true" },
-            },
-            };
-        }
 
-        private async Task<APIGatewayHttpApiV2ProxyResponse> CreateMovieAsync(APIGatewayHttpApiV2ProxyRequest request)
-        {
-            // Your existing logic for creating a movie
-            var movieRequest = JsonConvert.DeserializeObject<Movie>(request.Body);
-            movieRequest.Id = GenerateUniqueId();
-            await _dbContext.SaveAsync(movieRequest);
-
-            var message = $"Movie with Id {movieRequest?.Id} Created";
-            LambdaLogger.Log(message);
-
-            return new APIGatewayHttpApiV2ProxyResponse
-            {
-                Body = message,
-                StatusCode = 200,
-                Headers = new Dictionary<string, string>
-            {
-                { "Access-Control-Allow-Origin", "http://localhost:3000" },
-                { "Access-Control-Allow-Credentials", "true" },
-            },
-            };
-        }
-
-        private async Task<APIGatewayHttpApiV2ProxyResponse> GetMovieByIdAsync(APIGatewayHttpApiV2ProxyRequest request)
-        {
-            // Your existing logic for getting a movie by ID
-            if (!request.PathParameters.TryGetValue("id", out var id) || !int.TryParse(id, out var id_int))
+                var movieRequest = JsonConvert.DeserializeObject<Movie>(request.Body);
+                movieRequest.Id = GenerateUniqueId();
+                await _dbContext.SaveAsync(movieRequest);
+                var message = $"Movie with Id {movieRequest?.Id} Created";
+                LambdaLogger.Log(message);
+                return new APIGatewayHttpApiV2ProxyResponse
+                {
+                    Body = message,
+                    StatusCode = 200
+                };
+            }
+            catch (Exception ex)
             {
                 return new APIGatewayHttpApiV2ProxyResponse
                 {
-                    Body = "Invalid movie ID",
-                    StatusCode = 400,
-                    Headers = new Dictionary<string, string>
-                {
-                    { "Access-Control-Allow-Origin", "http://localhost:3000" },
-                    { "Access-Control-Allow-Credentials", "true" },
-                },
+                    Body = ex.Message,
+                    StatusCode = 400
                 };
             }
-
-            var movie = await _dbContext.LoadAsync<Movie>(id_int);
-            if (movie == null)
-            {
-                return new APIGatewayHttpApiV2ProxyResponse
-                {
-                    Body = "Movie not found",
-                    StatusCode = 404,
-                    Headers = new Dictionary<string, string>
-                {
-                    { "Access-Control-Allow-Origin", "http://localhost:3000" },
-                    { "Access-Control-Allow-Credentials", "true" },
-                },
-                };
-            }
-
-            return new APIGatewayHttpApiV2ProxyResponse
-            {
-                Body = JsonConvert.SerializeObject(movie),
-                StatusCode = 200,
-                Headers = new Dictionary<string, string>
-                {
-                    { "Access-Control-Allow-Origin", "http://localhost:3000" },
-                    { "Access-Control-Allow-Credentials", "true"
-                    },
-                }
-                };
         }
 
-        // Add other methods as needed
-
-        private static int GenerateUniqueId()
+        public static int GenerateUniqueId()
         {
             Guid guid = Guid.NewGuid();
             byte[] bytes = guid.ToByteArray();
+
+            // Take the first 4 bytes and convert them to an integer
             int uniqueId = BitConverter.ToInt32(bytes, 0);
+
             return uniqueId;
         }
 
@@ -191,21 +86,6 @@ namespace AWSLambda4
         {
             try
             {
-                if (request.RequestContext.Http.Method == "OPTIONS")
-                {
-                    // Handling preflight OPTIONS request for CORS
-                    return new APIGatewayHttpApiV2ProxyResponse
-                    {
-                        StatusCode = 200,
-                        Headers = new Dictionary<string, string>
-                {
-                    { "Access-Control-Allow-Origin", "http://localhost:3000" },
-                    { "Access-Control-Allow-Headers", "Content-Type" },
-                    { "Access-Control-Allow-Methods", "OPTIONS,POST,GET" },
-                    { "Access-Control-Allow-Credentials", "true" },
-                },
-                    };
-                }
 
                 var movieUpdateRequest = JsonConvert.DeserializeObject<Movie>(request.Body);
 
@@ -222,12 +102,7 @@ namespace AWSLambda4
                     return new APIGatewayHttpApiV2ProxyResponse
                     {
                         Body = "Movie not found",
-                        StatusCode = 404,
-                        Headers = new Dictionary<string, string>
-                {
-                    { "Access-Control-Allow-Origin", "http://localhost:3000" },
-                    { "Access-Control-Allow-Credentials", "true" },
-                },
+                        StatusCode = 404
                     };
                 }
 
@@ -282,7 +157,8 @@ namespace AWSLambda4
                     existingMovie.BackdropURL = movieUpdateRequest.BackdropURL;
                 }
 
-                // Update other fields similarly
+
+                // Add more fields to update as needed
 
                 // Save the updated movie record back to DynamoDB
                 await dbContext.SaveAsync(existingMovie);
@@ -293,12 +169,7 @@ namespace AWSLambda4
                 return new APIGatewayHttpApiV2ProxyResponse
                 {
                     Body = message,
-                    StatusCode = 200,
-                    Headers = new Dictionary<string, string>
-            {
-                { "Access-Control-Allow-Origin", "http://localhost:3000" },
-                { "Access-Control-Allow-Credentials", "true" },
-            },
+                    StatusCode = 200
                 };
             }
             catch (Exception ex)
@@ -306,30 +177,49 @@ namespace AWSLambda4
                 return new APIGatewayHttpApiV2ProxyResponse
                 {
                     Body = ex.Message,
-                    StatusCode = 400,
-                    Headers = new Dictionary<string, string>
-            {
-                { "Access-Control-Allow-Origin", "http://localhost:3000" },
-                { "Access-Control-Allow-Credentials", "true" },
-            },
+                    StatusCode = 400
                 };
             }
         }
 
 
-              
-               
+        public async Task<APIGatewayHttpApiV2ProxyResponse> GetMovieByIdAsync(APIGatewayHttpApiV2ProxyRequest request, ILambdaContext context)
+        {
+            try
+            {
+                if (!request.PathParameters.TryGetValue("id", out var id) || !int.TryParse(id, out var id_int))
+                {
+                    return new APIGatewayHttpApiV2ProxyResponse
+                    {
+                        Body = "Invalid movie ID",
+                        StatusCode = 400
+                    };
+                }
 
+                var movie = await _dbContext.LoadAsync<Movie>(id_int);
+                if (movie == null)
+                {
+                    return new APIGatewayHttpApiV2ProxyResponse
+                    {
+                        Body = "Movie not found",
+                        StatusCode = 404
+                    };
+                }
 
-                
-
-
+                return new APIGatewayHttpApiV2ProxyResponse
+                {
+                    Body = JsonConvert.SerializeObject(movie),
+                    StatusCode = 200
+                };
+            }
+            catch (Exception ex)
+            {
+                return new APIGatewayHttpApiV2ProxyResponse
+                {
+                    Body = ex.Message,
+                    StatusCode = 500
+                };
+            }
+        }
     }
-
-
 }
-
-
-
-
-
